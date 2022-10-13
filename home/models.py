6b2569150cc9dabaf5c8e6c49236ebe96081e2f9
Edward Gomez <edward@gomez.me.uk>
2022-10-13 14:58:26 +0100

    Language settings and styles

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+from django.core.paginator import Paginator
 from django.db import models
 
 from wagtail.admin.panels import FieldPanel
@@ -6,10 +8,21 @@ from wagtail.contrib.table_block.blocks import TableBlock
 from wagtail.fields import RichTextField, StreamField
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.models import Page
+from wagtail.models import Page, Locale
 from wagtail.search import index
 
 from wagtail import blocks
+
+def stream_teaser(content, title):
+    if content:
+        html = content[0].value.source
+        soup = BeautifulSoup(html, 'html.parser')
+        txt = soup.text
+        if len(txt) > 200:
+            txt = f"{txt[0:200]}..."
+        return txt
+    else:
+        return f"{title} on Las Cumbres Observatory"
 
 
 class ImageText(StructBlock):
@@ -27,8 +40,8 @@ class BodyBlock(StreamBlock):
 
 class ContentPage(Page):
     summary = RichTextField("optional summary/teaser", blank=True)
-    content = StreamField(BodyBlock)
-    extra_info = StreamField(BodyBlock, help_text ="Info normally appears on right side, above links", blank=True)
+    content = StreamField(BodyBlock, use_json_field=True)
+    extra_info = StreamField(BodyBlock, help_text ="Info normally appears on right side, above links", blank=True, use_json_field=True)
     featured_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -53,19 +66,21 @@ class NewsPage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    teaser = models.CharField("Optional teaser", max_length=250, blank=True)
-    content = StreamField(BodyBlock)
+    summary = models.CharField("Optional teaser", max_length=250, blank=True)
+    content = StreamField(BodyBlock, use_json_field=True)
 
     search_fields = Page.search_fields + [
-        index.SearchField('description'),
+        index.SearchField('teaser'),
         index.SearchField('content'),
     ]
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('featured_image'),
-        FieldPanel('teaser'),
+        FieldPanel('summary'),
         FieldPanel('content'),
     ]
+
+    parent_page_types = ['home.NewsIndexPage']
 
     @property
     def display_date(self):
@@ -74,6 +89,13 @@ class NewsPage(Page):
         else:
             return self.first_published_at
 
+    @property
+    def teaser(self):
+        if self.summary:
+            return self.summary
+        else:
+            return stream_teaser(self.content, self.title)
+
 
 class NewsIndexPage(Page):
     description = RichTextField(blank=True)
@@ -81,7 +103,7 @@ class NewsIndexPage(Page):
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
-        blog_list = NewsPage.objects.live().exclude(categories=ann).order_by('-go_live_at')
+        blog_list = NewsPage.objects.filter(locale=Locale.get_active()).live().order_by('-go_live_at')
         paginator = Paginator(blog_list, 20)
         page = request.GET.get('page')
         blogpages = paginator.get_page(page)
@@ -91,4 +113,21 @@ class NewsIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('description'),
     ]
-    parent_page_types = ['home.ContentPage']
+    parent_page_types = ['home.ContentPage','home.HomePage']
+
+class HomePage(Page):
+    tagline = RichTextField("short description", blank=True)
+    about = RichTextField("about project", blank=True)
+    content = StreamField(BodyBlock, use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('tagline'),
+        FieldPanel('about'),
+        FieldPanel('content'),
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        context['news'] = NewsPage.objects.live().order_by('-go_live_at')[2]
+        return context
