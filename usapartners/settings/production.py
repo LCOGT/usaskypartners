2227b5e8896a3186a5f8d2b8958130e79fb2b569
Edward Gomez <edward@gomez.me.uk>
2022-10-18 09:34:33 +0100

    Adding db and secret to production settings

--- usapartners/settings/production.py
+++ usapartners/settings/production.py
@@ -1,6 +1,29 @@
 from .base import *
+import ast
+import sys
+import os
 
-DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+WAGTAILADMIN_BASE_URL = "https://usaskypartners.lco.global"
+
+DEBUG = ast.literal_eval(os.environ.get('DEBUG', 'False'))
+if DEBUG:
+    print('WARNING: DEBUG mode is turned on in PRODUCTION!', file=sys.stderr)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'HOST': os.environ['DB_HOST'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'OPTIONS': {
+        },
+    }
+}
 
 try:
     from .local import *
